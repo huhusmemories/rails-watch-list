46f02f6adb1c3510f9c3a5907e6bae545b6022f9
Huang Shewei <huangshewei@gmail.com>
2022-02-17 15:18:02 -0500

    Seed file contains faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "Destroying previous DB"
+Movie.destroy_all
+
+puts "Populating database..."
+
+10.times do
+  movie = Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote
+  )
+  puts "Movie: #{movie.id} was created"
+end
+puts "Done"
