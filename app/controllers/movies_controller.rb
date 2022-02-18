f2e13b813b363081a7bf22155863a9efde15bdff
Huang Shewei <huangshewei@gmail.com>
2022-02-18 11:16:58 -0500

    Add movie & access show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,24 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
-  def create
+  def show
+    @movie = Movie.find(params[:id])
+  end
 
+  def create
+    @movie = Movie.new(movie_params)
+    # raise
+    if @movie.save
+      redirect_to @movie, notice: 'Movie added to the list'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url)
   end
 end
