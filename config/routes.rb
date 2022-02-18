8331e2876c23447f3266428c1e07c55f2ae825ae
Huang Shewei <huangshewei@gmail.com>
2022-02-18 15:05:08 -0500

    Adding bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'movies#index'
-  resources :movies, :lists
+  root to: 'lists#index'
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
+# Need a route for bookmark
+# New method and create
+# Nested resources for bookmarks (new and create)
+# Delete doesnt need to be nested
